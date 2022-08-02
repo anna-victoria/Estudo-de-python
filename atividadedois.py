583af3b42261a4480ac6d4798c70732d9da1cf65
Anna Victoria Feliciana <anna.feliciana@upe.br>
2022-08-01 22:37:06 -0300

    Initial commit

---
+++ atividadedois.py
@@ -0,0 +1,5 @@
+dia = input('Informe o dia: \n')
+mes = input('Informe o mes: \n')
+ano = input('Informe o ano: \n')
+
+print('A data informada foi: ' + dia + "/" + mes + "/" + ano)
---
+++ atividadetres.py
@@ -0,0 +1,6 @@
+print('Olá, vamos fazer a soma de dois números')
+numUm = input('Insira um número: \n')
+numDois = input('Insira outro número: \n')
+
+soma = int(numUm) + int(numDois)
+print('O resultado de sua soma é: ' + str(soma))
